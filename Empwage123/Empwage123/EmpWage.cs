a19c48a737501b350a51928e1bad202eb6b815df
Dipak@1432 <chavandipakr17@gmail.com>
2022-01-22 15:15:03 +0530

    PartTime

--- Empwage123/Empwage123/EmpWage.cs
+++ Empwage123/Empwage123/EmpWage.cs
@@ -11,12 +11,17 @@ namespace Empwage123
         public static void EmpAttendance()
         {
             int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             int empHr = 0;
             int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+            {
+                empHr = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
             {
                 empHr = 8;
             }
