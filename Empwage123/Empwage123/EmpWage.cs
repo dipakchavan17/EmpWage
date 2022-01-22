c3c0e9c1de59e7a24eee04be72ceb008e6a3aa06
Dipak@1432 <chavandipakr17@gmail.com>
2022-01-22 15:19:22 +0530

    Switch case employee

--- Empwage123/Empwage123/EmpWage.cs
+++ Empwage123/Empwage123/EmpWage.cs
@@ -8,36 +8,40 @@ namespace Empwage123
 {
     internal class EmpWage
     {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
         public static void EmpAttendance()
         {
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
+          
+       
+
             int empHr = 0;
             int empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_PART_TIME)
+            switch (empCheck)
             {
-                empHr = 4;
-            }
-            else if (empCheck == IS_FULL_TIME)
-            {
-                empHr = 8;
-            }
-            else
-            {
-                empHr = 0;
+                case IS_PART_TIME:
+                    empHr = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHr = 8;
+                    break;
+                default:
+                    empHr = 0;
+                    break;
             }
             empWage = empHr * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage" + empWage);
             Console.ReadLine();
-
         }
 
+    }
 
-    }
-    }
+
+}
+    
 
 
 
