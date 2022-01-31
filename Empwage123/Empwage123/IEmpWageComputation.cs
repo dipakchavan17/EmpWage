fc232dbc80c48971f50b18a814938510f18667c5
Dipak@1432 <chavandipakr17@gmail.com>
2022-01-31 13:56:24 +0530

    Store total wage

---
+++ Empwage123/Empwage123/IEmpWageComputation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empwage123
+{
+    //Interface
+    public interface IEmpWageCompute
+    {
+        void AddCompany(string company, int wagePrHrs, int totalWorkHrs, int totalWorkDay);
+        void GetWage();
+    }
+}
